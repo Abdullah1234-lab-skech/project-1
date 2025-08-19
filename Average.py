c4e09319d6b0c89cf151b684a60322f351d175f9
Abdullah1234-lab-skech <abdullahhussainn8847@gmail.com>
2025-08-19 20:22:42 +0300

    first commit

---
+++ Average.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number whose sum you want to find ")) 
+sum=0
+for i in range(1, n+1):
+    sum=sum+i 
+print("The sum =", sum) 
+string=input("Enter a string: ")
+string2=('') 
+for i in string: 
+    string2= i + string2 
+print("/nThe Original String is=", string) 
+print("The Reversed String is=", string2)
