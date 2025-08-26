2b3d5376de3a481a42eae12536014fe285aca2a1
Abdullah1234-lab-skech <abdullahhussainn8847@gmail.com>
2025-08-26 20:58:34 +0300

    Power Calculator

---
+++ power.py
@@ -0,0 +1,6 @@
+Number=int(input("Enter a number:")) 
+power=int(input("Enter the power of the number you entered :")) 
+Result=1 
+for i in range(power):
+ Result=power**2  
+print("The result is:",Result) 
